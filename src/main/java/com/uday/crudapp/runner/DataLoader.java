c9fb9b9e94b41cc9e5720a0251893aa29857572a
uday <udaykiran.kuk70@gmail.com>
2025-12-24 10:53:59 +0530

    Day 3: Added StudentRepository and data loader

---
+++ src/main/java/com/uday/crudapp/runner/DataLoader.java
@@ -0,0 +1,19 @@
+package com.uday.crudapp.runner;
+
+import com.uday.crudapp.entity.Student;
+import com.uday.crudapp.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataLoader {
+
+    @Bean
+    CommandLineRunner loadData(StudentRepository repository) {
+        return args -> {
+            Student s = new Student("Uday", "uday@gmail.com", "Java");
+            repository.save(s);
+        };
+    }
+}
